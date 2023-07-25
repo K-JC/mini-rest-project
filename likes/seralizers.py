9cdf5179baa704c1ec55a368c53c5b217a9e2bfe
K-JC <kellytolfts93@hotmail.com>
2023-07-25 09:58:07 +0000

    add integrity error to likes serializer

--- likes/seralizers.py
+++ likes/seralizers.py
@@ -1,4 +1,7 @@
-from .models import Like
+from django.db import IntegrityError
+from rest_framework import serializers
+from likes.models import Like
+
 
 class LikeSerializer(serializers.ModelSerializer):
     """
@@ -10,3 +13,11 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
